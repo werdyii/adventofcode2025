5e365586c109d1255fe4dd9f352c754fd79a8d31
Andrej Klucka <andrej.klucka@gmail.com>
2025-12-13 00:45:20 +0100

    Day01Puzzle2

--- src/Day01/Puzzle2.php
+++ src/Day01/Puzzle2.php
@@ -13,6 +13,56 @@ class Puzzle2
             ?: throw new Exception('Failed to read input file.');
 
         // TODO: Solve puzzle 2.
-        return new Collection(explode("\n", $input));
+        echo "The dial starts by pointing at 50.\n";
+        return (new Collection(explode("\n", $input)))->reduce(function ($state, $move) {
+
+            if ($move === '') {
+                return $state;
+            }
+
+            echo "The dial is rotated [$move]";
+
+            // Parsovanie
+            $direction = $move[0];
+            $value = (int) substr($move, 1);
+
+            // Delta cez ternár (bez if-u)
+            $delta = ($direction === 'R') ? $value : -$value;
+            
+            $current = $state['current'];
+            $zeroHits = $state['zeroHits']; 
+
+            // 1. plné rotácie
+            $zeroHits += intdiv(abs($delta), 100);
+
+            // 2. zvyšok. Pohyb (0-99) a kontrola prechodu cez 0
+            $remainder = $delta % 100;
+
+            if ($remainder !== 0) {
+                $start = $current;
+                $end   = $current + $remainder;
+
+                if (
+                    ($remainder > 0 && $start < 100 && $end >= 100) ||
+                    ($remainder < 0 && $start > 0   && $end <= 0)
+                ) {
+                    $zeroHits++;
+                }
+            }
+
+            // 3. finálna pozícia
+            $current = (($current + $delta) % 100 + 100) % 100;
+            echo " to position at [$current]. Total zero hits so far: [$zeroHits].\n";
+
+            return [
+                'current' => $current,
+                'zeroHits' => $zeroHits,
+            ];
+
+        }, [
+            'current' => 50,
+            'zeroHits' => 0,
+        ])['zeroHits'];
+
     }
 }
